2029df8a42bff6431cbaab6f97d3386ac01218ae
koya-namba <namba.koya@arakawa-lab.com>
2022-12-15 13:10:18 +0900

    Modify css

--- resources/views/answers/create.blade.php
+++ resources/views/answers/create.blade.php
@@ -1,28 +1,69 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create') }}
+            {{ __('Answer Create') }}
         </h2>
     </x-slot>
-    <h1>質問題名: {{ $question->title }}</h1>
-    <h3>
-        カテゴリー: 
-            @foreach($question->categories as $category)
-                {{ $category->category_name }}
-            @endforeach
-    </h3>
-    <h3>質問者: {{ $question->user->name }}</h3>
-    <h3>トレーニング歴: </h3>
-    <h3>投稿日時: </h3>
-    <h2>質問内容</h2>
-    <p>{{ $question->context }}</p>
-    <br />
-    <form action="{{ route('answers.store', $question) }}" method="POST">
-        @csrf
-        <h3>質問者:{{ Auth::user()->name }}</h3>
-        <h3>回答内容</h3>
-        <textarea name="answer[context]"></textarea>
-        <button type="submit">Submit</button>
-    </form>
-    <a href="{{ route('questions.show', $question) }}">キャンセル</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="mt-2">
+                        <h1>質問タイトル: {{ $question->title }}</h1>
+                    </div>
+                    <div class="mt-2">
+                        <h3>
+                            カテゴリー: 
+                            @foreach($question->categories as $category)
+                                {{ $category->category_name }}
+                            @endforeach
+                        </h3>
+                    </div>
+                    <div class="mt-2">
+                        <h3>質問者: {{ $question->user->name }}</h3>
+                    </div>
+                    <div class="mt-2">
+                        <h3>トレーニング歴: </h3>
+                    </div>
+                    <div class="mt-2">
+                        <h3>投稿日時: </h3>
+                    </div>
+                    <div class="mt-2">
+                        <h2>質問内容</h2>
+                    </div>
+                    <div class="mt-1">
+                        <p>{{ $question->context }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <form action="{{ route('answers.store', $question) }}" method="POST">
+                            @csrf
+                            <div class="mt-2">
+                                <h3>回答者:{{ Auth::user()->name }}</h3>
+                            </div>
+                            <div class="mt-2">
+                                <h3>回答内容</h3>
+                            </div>
+                            <div class="mt-1">
+                                <textarea name="answer[context]"></textarea>
+                            </div>
+                            <div class="mt-4 flex">
+                                <button type="submit" class="bg-blue-500 rounded font-medium px-4 py-2 text-white">
+                                    回答する
+                                </button>
+                                <div class="p-2">
+                                    <a href="{{ route('questions.show', $question) }}" class="bg-blue-500 rounded font-medium px-4 py-2 text-white">
+                                        キャンセル
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -1,23 +1,49 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create') }}
+            {{ __('Question Create') }}
         </h2>
     </x-slot>
-    <form action="{{ route('questions.store') }}" method="POST">
-        @csrf
-        <h1>質問題名</h1>
-        <textarea name="question[title]"></textarea>
-        <h3>カテゴリー</h3>
-        @foreach($categories as $category)
-            <input type="checkbox" value="{{ $category->id }}" name="categories_arr[]">
-                {{ $category->category_name }}
-            </input>
-        @endforeach
-        <h3>質問者:{{ Auth::user()->name }}</h3>
-        <h3>質問内容</h3>
-        <textarea name="question[context]"></textarea>
-        <button type="submit">Submit</button>
-    </form>
-    <a href="{{ route('questions.index') }}">キャンセル</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ route('questions.store') }}" method="POST">
+                        @csrf
+                        <div class="mt-2">
+                            <h1>質問タイトル</h1>
+                            <textarea name="question[title]"></textarea>
+                        </div>
+                        <div class="mt-2">
+                            <h3>カテゴリー</h3>
+                            @foreach($categories as $category)
+                                <input type="checkbox" value="{{ $category->id }}" name="categories_arr[]">
+                                    {{ $category->category_name }}
+                                </input>
+                            @endforeach
+                        </div>
+                        <div class="mt-2">
+                            <h3>質問者:{{ Auth::user()->name }}</h3>
+                        </div>
+                        <div class="mt-2">
+                            <h3>質問内容</h3>
+                        </div>
+                        <div class="mt-2">
+                            <textarea name="question[context]"></textarea><br />
+                        </div>
+                        <div class="mt-4 flex">
+                            <button type="submit" class="bg-blue-500 rounded font-medium px-4 py-2 text-white">
+                                質問する
+                            </button>
+                            <div class="p-2">
+                                <a href="{{ route('questions.index') }}" class="bg-blue-500 rounded font-medium px-4 py-2 text-white">
+                                    キャンセル
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
